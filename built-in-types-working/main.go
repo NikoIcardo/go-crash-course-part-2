b7fa3059d8895dd2a272ca8311d32ad649d7d290
Niko Icardo <nikoicardo@Nikos-MacBook-Pro.local>
2024-06-24 16:39:02 -0700

    Experiment with interfaces

--- built-in-types-working/main.go
+++ built-in-types-working/main.go
@@ -1,41 +1,62 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/eiannone/keyboard"
-)
-
-var keyPressChan chan rune
-
-func main() {
-	keyPressChan = make(chan rune)
-
-	go listenForKeyPress()
-
-	fmt.Println(" Press any key, or q to quit")
-
-	_ = keyboard.Open()
-
-	defer func() {
-		keyboard.Close()
-	}()
-
-	for {
-		char, _, _ := keyboard.GetSingleKey()
-
-		if char == 'q' || char == 'Q' {
-			break
-		}
-
-		keyPressChan <- char
-	}
+type Animal interface {
+	Says() string
+	HowManyLegs() int
 }
 
-func listenForKeyPress() {
-	for {
-		key := <-keyPressChan
+type Dog struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+}
 
-		fmt.Println("You pressed", string(key))
+func (d *Dog) Says() string {
+	return d.Sound
+}
+
+func (d *Dog) HowManyLegs() int {
+	return d.NumberOfLegs
+}
+
+type Cat struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+	HasTail      bool
+}
+
+func (d *Cat) Says() string {
+	return d.Sound
+}
+
+func (d *Cat) HowManyLegs() int {
+	return d.NumberOfLegs
+}
+
+func main() {
+	dog := Dog{
+		Name:         "dog",
+		Sound:        "woof",
+		NumberOfLegs: 4,
 	}
+
+	riddle(&dog)
+
+	var cat Cat
+
+	cat.Name = "cat"
+
+	cat.NumberOfLegs = 4
+	cat.HasTail = true
+	cat.Sound = "meow"
+
+	riddle(&cat)
+}
+
+func riddle(a Animal) {
+	riddle := fmt.Sprintf(`This animal says "%s" and has %d legs. What animal is it?`, a.Says(), a.HowManyLegs())
+	fmt.Println(riddle)
 }
